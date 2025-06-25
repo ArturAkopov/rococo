140c9e774a30f17d6de100c4247d30bd664759fe
Artur <116442638+ArturAkopov@users.noreply.github.com>
2025-06-25 15:36:00 +0300

    Contract test: ArtistControllerGrpcTest 1. Получение данных 2. Создание и обновление 3. Проверка структуры и содержимого ответов (полей объектов) 4. Проверка работы с WireMock для эмуляции gRPC сервиса 5. Обработка ошибок gRPC 6. Проверка корректности формирования ошибок (структура, сообщения) 7. Валидация входных данных 8. Безопасность и авторизация

--- rococo-gateway/src/main/java/anbrain/qa/rococo/exception/GlobalExceptionHandler.java
+++ rococo-gateway/src/main/java/anbrain/qa/rococo/exception/GlobalExceptionHandler.java
@@ -3,9 +3,11 @@ package anbrain.qa.rococo.exception;
 import anbrain.qa.rococo.controller.error.ApiError;
 import jakarta.annotation.Nonnull;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -45,6 +47,51 @@ public class GlobalExceptionHandler {
         );
     }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleBadRequestException(ConstraintViolationException ex, @Nonnull HttpServletRequest servletRequest) {
+        log.error(servletRequest.getRequestURI(), ex);
+        return new ResponseEntity<>(
+                new ApiError(
+                        HttpStatus.BAD_REQUEST.toString(),
+                        servletRequest.getRequestURI(),
+                        "Bad request",
+                        ex.getMessage()
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler(RococoAccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<ApiError> handleAccessDeniedException(RococoAccessDeniedException ex, @Nonnull HttpServletRequest servletRequest) {
+        log.error(servletRequest.getRequestURI(), ex);
+        return new ResponseEntity<>(
+                new ApiError(
+                        HttpStatus.FORBIDDEN.toString(),
+                        servletRequest.getRequestURI(),
+                        "Forbidden",
+                        ex.getMessage()
+                ),
+                HttpStatus.FORBIDDEN
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiError> handleBadRequestException(MethodArgumentNotValidException ex, @Nonnull HttpServletRequest servletRequest) {
+        log.error(servletRequest.getRequestURI(), ex);
+        return new ResponseEntity<>(
+                new ApiError(
+                        HttpStatus.BAD_REQUEST.toString(),
+                        servletRequest.getRequestURI(),
+                        "Bad request",
+                        ex.getMessage()
+                ),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
     @ExceptionHandler(RococoConflictException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public ResponseEntity<ApiError> handleConflictException(RococoConflictException ex, @Nonnull HttpServletRequest servletRequest) {
@@ -57,6 +104,21 @@ public class GlobalExceptionHandler {
                         ex.getMessage()
                 ),
                 HttpStatus.CONFLICT
+        );
+    }
+
+    @ExceptionHandler(RococoServiceUnavailableException.class)
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ResponseEntity<ApiError> handleUnavailableException(RococoServiceUnavailableException ex, @Nonnull HttpServletRequest servletRequest) {
+        log.error(servletRequest.getRequestURI(), ex);
+        return new ResponseEntity<>(
+                new ApiError(
+                        HttpStatus.SERVICE_UNAVAILABLE.toString(),
+                        servletRequest.getRequestURI(),
+                        "Service Unavailable",
+                        ex.getMessage()
+                ),
+                HttpStatus.SERVICE_UNAVAILABLE
         );
     }
 
--- rococo-gateway/src/test/java/anbrain/qa/rococo/controller/ArtistControllerGrpcTest.java
+++ rococo-gateway/src/test/java/anbrain/qa/rococo/controller/ArtistControllerGrpcTest.java
@@ -8,6 +8,7 @@ import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,11 +27,13 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.wiremock.grpc.dsl.WireMockGrpc.Status.*;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@Tag("ContractTest")
 class ArtistControllerGrpcTest {
 
     private static final int WIREMOCK_PORT = 9091;
@@ -92,7 +95,7 @@ class ArtistControllerGrpcTest {
                         .willReturn(WireMockGrpc.message(response)));
 
         mockMvc.perform(get("/api/artist")
-                        .with(jwt().jwt(c -> c.claim("sub", "duck")))
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
                         .param("page", "0")
                         .param("size", "10"))
                 .andExpect(status().isOk())
@@ -117,7 +120,7 @@ class ArtistControllerGrpcTest {
                         .willReturn(WireMockGrpc.message(response)));
 
         mockMvc.perform(get("/api/artist/{id}", artistID)
-                        .with(jwt().jwt(c -> c.claim("sub", "duck"))))
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur"))))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", Matchers.is(artistID)))
                 .andExpect(jsonPath("$.name", Matchers.is("Шишкин")))
@@ -139,7 +142,7 @@ class ArtistControllerGrpcTest {
                         .willReturn(WireMockGrpc.message(response)));
 
         mockMvc.perform(get("/api/artist")
-                        .with(jwt().jwt(c -> c.claim("sub", "duck")))
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
                         .param("name", "Шиш")
                         .param("page", "0")
                         .param("size", "10"))
@@ -162,7 +165,7 @@ class ArtistControllerGrpcTest {
                         .willReturn(WireMockGrpc.message(response)));
 
         mockMvc.perform(post("/api/artist")
-                        .with(jwt().jwt(c -> c.claim("sub", "duck")))
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(status().isCreated())
@@ -186,12 +189,176 @@ class ArtistControllerGrpcTest {
                         .willReturn(WireMockGrpc.message(response)));
 
         mockMvc.perform(put("/api/artist")
-                .with(jwt().jwt(c -> c.claim("sub", "duck")))
+                .with(jwt().jwt(c -> c.claim("sub", "Artur")))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", Matchers.is("104f76ce-0508-49d4-8967-fdf1ebb8cf45")))
                 .andExpect(jsonPath("$.biography", Matchers.is("Обновленная биография")))
                 .andExpect(jsonPath("$.name", Matchers.is("Обновленный Шишкин")));
+    }
+
+    @Test
+    void getArtistById_ShouldReturn404WhenArtistNotFound() throws Exception {
+        mockArtistService.stubFor(
+                WireMockGrpc.method("GetArtist")
+                        .willReturn(NOT_FOUND,"Художник с ID "+artistID+" не найден"));
+
+        mockMvc.perform(get("/api/artist/{id}", artistID)
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur"))))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error.code", Matchers.is("404 NOT_FOUND")))
+                .andExpect(jsonPath("$.error.errors[0].domain",
+                        Matchers.is("/api/artist/" + artistID)))
+                .andExpect(jsonPath("$.error.errors[0].reason",
+                        Matchers.is("Not found")))
+                .andExpect(jsonPath("$.error.errors[0].message",
+                        Matchers.is("Художник с ID " + artistID + " не найден")));
+    }
+
+    @Test
+    void getArtistById_ShouldReturn400WhenInvalidIdFormat() throws Exception {
+        mockMvc.perform(get("/api/artist/{id}", "invalid-uuid")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur"))))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error.code", Matchers.is("400 BAD_REQUEST")))
+                .andExpect(jsonPath("$.error.errors[0].domain",
+                        Matchers.is("/api/artist/invalid-uuid")))
+                .andExpect(jsonPath("$.error.errors[0].reason",
+                        Matchers.is("Bad request")))
+                .andExpect(jsonPath("$.error.errors[0].message",
+                        Matchers.containsString("Неверный формат ID музея: invalid-uuid")));
+    }
+
+    @Test
+    void createArtist_ShouldReturn409WhenArtistExists() throws Exception {
+        final String requestJson = loadRequestJson(WIREMOCK_ROOT+REQUEST_PATH+"create_artist_request.json");
+
+        mockArtistService.stubFor(
+                WireMockGrpc.method("CreateArtist")
+                        .willReturn(ALREADY_EXISTS, "Артист уже существует"));
+
+        mockMvc.perform(post("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$.error.code", Matchers.is("409 CONFLICT")))
+                .andExpect(jsonPath("$.error.errors[0].domain",
+                        Matchers.is("/api/artist")))
+                .andExpect(jsonPath("$.error.errors[0].reason",
+                        Matchers.is("Conflict")))
+                .andExpect(jsonPath("$.error.errors[0].message",
+                        Matchers.is("Создание художника с такими параметрами уже существует: Новый художник")));
+    }
+
+    @Test
+    void updateArtist_ShouldReturn404WhenArtistNotFound() throws Exception {
+        final String requestJson = loadRequestJson(WIREMOCK_ROOT+REQUEST_PATH+"update_artist_request.json");
+
+        mockArtistService.stubFor(
+                WireMockGrpc.method("UpdateArtist")
+                        .willReturn(NOT_FOUND, "Художник не найден"));
+
+        mockMvc.perform(put("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error.code", Matchers.is("404 NOT_FOUND")))
+                .andExpect(jsonPath("$.error.errors[0].domain",
+                        Matchers.is("/api/artist")))
+                .andExpect(jsonPath("$.error.errors[0].reason",
+                        Matchers.is("Not found")))
+                .andExpect(jsonPath("$.error.errors[0].message",
+                        Matchers.is("Обновление художника с ID 104f76ce-0508-49d4-8967-fdf1ebb8cf45 не найден")));
+    }
+
+    @Test
+    void createArtist_ShouldReturn400WhenBiographyIsTooLong() throws Exception {
+        String longBio = "a".repeat(2001);
+        String requestJson = String.format("{\"name\":\"Valid name\", \"biography\":\"%s\", \"photo\":\"data:image/jpeg;base64,valid\"}", longBio);
+
+        mockMvc.perform(post("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error.code", Matchers.is("400 BAD_REQUEST")));
+    }
+
+    @Test
+    void getAllArtists_ShouldReturn503WhenGrpcServiceUnavailable() throws Exception {
+        mockArtistService.stubFor(
+                WireMockGrpc.method("GetAllArtists")
+                        .willReturn(UNAVAILABLE, "Сервис недоступен"));
+
+        mockMvc.perform(get("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isServiceUnavailable())
+                .andExpect(jsonPath("$.error.errors[0].message", Matchers.is("Сервис временно недоступен")));
+    }
+
+    @Test
+    void getArtistById_ShouldReturn504WhenGrpcTimeout() throws Exception {
+        mockArtistService.stubFor(
+                WireMockGrpc.method("GetArtist")
+                        .willReturn(DEADLINE_EXCEEDED, "Таймаут"));
+
+        mockMvc.perform(get("/api/artist/{id}", artistID)
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur"))))
+                .andExpect(status().isServiceUnavailable())
+                .andExpect(jsonPath("$.error.errors[0].message", Matchers.is("Превышено время ожидания ответа от сервиса")));
+    }
+
+    @Test
+    void searchArtistsByName_ShouldReturn400WhenNameIsBlank() throws Exception {
+        mockMvc.perform(get("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .param("name", "")
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error.code", Matchers.is("400 BAD_REQUEST")));
+    }
+
+    @Test
+    void getAllArtists_ShouldReturn500WhenUnexpectedGrpcError() throws Exception {
+        mockArtistService.stubFor(
+                WireMockGrpc.method("GetAllArtists")
+                        .willReturn(INTERNAL, "Внутренняя ошибка"));
+
+        mockMvc.perform(get("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.error.errors[0].message", Matchers.containsString("Произошла внутренняя ошибка сервера")));
+    }
+
+    @Test
+    void getAllArtists_ShouldReturn401WhenUnauthorized() throws Exception {
+        mockMvc.perform(get("/api/artist")
+                        .param("page", "0")
+                        .param("size", "10"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void createArtist_ShouldReturn403WhenForbidden() throws Exception {
+        final String requestJson = loadRequestJson(WIREMOCK_ROOT+REQUEST_PATH+"create_artist_request.json");
+
+        mockArtistService.stubFor(
+                WireMockGrpc.method("CreateArtist")
+                        .willReturn(WireMockGrpc.Status.PERMISSION_DENIED, "Доступ запрещен"));
+
+        mockMvc.perform(post("/api/artist")
+                        .with(jwt().jwt(c -> c.claim("sub", "Artur")))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.error.errors[0].message", Matchers.is("Доступ запрещен")));
     }
 }
